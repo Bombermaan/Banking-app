7c5fc81068737d1f3777f8c7ab3a71a4e24f936b
Bombermaan <Darkrayman1@gmail.com>
2023-04-12 11:32:09 +0300

    Added first version of creating and storing a user account

--- account.py
+++ account.py
@@ -1,21 +1,31 @@
 class Account:
-    def __init__(self, acctype, bal) -> None:
+    def __init__(self, username, password, acctype, bal) -> None:
+        self.username = username
+        self.password = password
         self.account_type = acctype
         self.balance = bal
         self.interest = 0
         if(self.account_type == "savings"):
-            self.interest = "4%"
+            self.interest = 0.04
         elif(self.account_type == "checking"):
-            self.interest = "2%"
+            self.interest = 0.02
         elif(self.account_type == "child"):
-            self.interest = "3%"
+            self.interest = 0.03
         elif(self.account_type == "brokerage"):
-            self.interest = "0.45%"
+            self.interest = 0.0045
     def addBalance(self,amt):
         self.balance += amt
         return f"Balance added to account. Amount added: {amt}, current balance: {self.balance}"
     def withdrawBalance(self,amt):
         self.balance -= amt
         return f"Withdrew {amt} from account. Your new balance is: {self.balance}"
+    def addInterest(self):
+        self.balance += self.balance*self.interest
+        return f"Interest added! New account balance is: {self.balance}"
+    def saveAccountState(self):
+        print("Saving account information...")
+        with open('accountInfo.txt', "w") as f:
+            f.write('Username: ' + self.username + '\nPassword: ' + self.password + '\nAccount type: ' + self.account_type + '\nBalance: ' + str(self.balance) + '\nInterest rate: ' + str(self.interest))
+        print("Done!")
     def __str__(self) -> str:
-        return f"You have a {self.account_type} account. Your interest rate is therefore {self.interest} and your balance is {self.balance}"
+        return f"You have a {self.account_type} account. Your interest rate is therefore {self.interest*100}% and your balance is {self.balance}"
