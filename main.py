5e671a3dffe82233912afa238a115e955e01dfe3
Bombermaan <Darkrayman1@gmail.com>
2023-04-12 12:57:00 +0300

    Finished account creating in main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from account import *
+import math
 
 
 def main():
     
     print("Hello and welcome to the Very Cool Banking App™. Do you want to log in to an existing account or create a new one?")
     while True:
-        userLoginChoice = (int(input("Press 1 to login(in development), press 2 to create account: ")))
-        if(userLoginChoice == 1):
+        user_login_choice = (int(input("Press 1 to login(in development), press 2 to create account: ")))
+        if(user_login_choice == 1):
             #Add account migration from file here
             #Use a dictionary to match key to value (username to password) for verification.
             login_username = input("Enter your username: ")
             login_password = input("Enter password: ")
             break
-        elif(userLoginChoice == 2):
+        elif(user_login_choice == 2):
             #Add account creation here
             print("Create your account here!")
             while True:
@@ -23,7 +24,48 @@ def main():
                     break
                 else:
                     print("Error! passwords did not match, try again!")
-                    continue    
+                    continue
+            while True:
+                print("The account types you can open are: 1. Savings, 2. Checking, 3. Child, 4. Brokerage")
+                user_choice = int(input("Enter the type of account you want to open (1 for savings, 2 for checking etc.)."))
+                user_account_type = ""
+                if(user_choice == 1):
+                    user_account_type = "savings"
+                    break
+                elif(user_choice == 2):
+                    user_account_type = "checking"
+                    break
+                elif(user_choice == 3):
+                    user_account_type = "child"
+                    break
+                elif(user_choice == 4):
+                    user_account_type = "brokerage"
+                    break
+                else:
+                    error = f"Error! Check input value! Please type a number from 1 to 4. Your value was {user_choice}."
+                    continue
+            print(f"You have chosen a {user_account_type} account.")
+            default_balance = 100
+            print("How much money will you put into this account? The minimum is 100.")
+            print("If you leave the space empty, it will assume the 100 and put that into the account.")
+            try:
+                balance = int(input("Please input the amount you will put into the account: "))
+                if balance < 100:
+                    print("Added balance too low, adjusting amount to 100")
+                    balance = 100
+            except ValueError:
+                print("Input empty, assuming default value of 100.")
+                balance = default_balance
+            print("Finally, create a nickname for your account:")
+            while True:
+                nickname = input("Input nickname: ")
+                if(nickname != ""):
+                    break
+                else:
+                    print("Error! Check nickname!")
+            nickname = Account(user_name, password, user_account_type, balance)
+            print(nickname)
+            nickname.saveAccountState()
             break
         else:
             error = f"Invalid value! Please press only 1 or 2. your input was {userLoginChoice}"
